1a2cfc76113961427c5e31f407fa531c115c5def
shimanto-rehman <shimanto.rehman.bd@gmail.com>
2026-02-05 18:47:08 +0600

    random

--- DisputeAutomation/Controllers/Dispute.cs
+++ DisputeAutomation/Controllers/Dispute.cs
@@ -1,6 +1,50 @@
-﻿namespace DisputeAutomation.Controllers
+using BusinessLogicLayer.Manager;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DisputeAutomation.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CollectionsController : ControllerBase
 {
-    public class Dispute
+    private readonly ICollectionManager _collectionManager;
+
+    public CollectionsController(ICollectionManager collectionManager)
     {
+        _collectionManager = collectionManager;
+    }
+
+    /// <summary>
+    /// Returns today's collections filtered by branch, client and status IDs.
+    /// Mirrors the following query:
+    /// SELECT * FROM [MTBBillCollection].[dbo].[Collection]
+    /// WHERE DATEDIFF(day, CollDate, GETDATE()) = 0
+    ///   AND CollStatusId IN (1)
+    ///   AND BranchCode = '0502'
+    ///   AND ClientId = 31
+    /// </summary>
+    [HttpGet("today")]
+    public async Task<IActionResult> GetTodayCollections(
+        [FromQuery] string branchCode,
+        [FromQuery] int clientId,
+        [FromQuery] List<int> collStatusIds,
+        CancellationToken cancellationToken)
+    {
+        if (string.IsNullOrWhiteSpace(branchCode))
+        {
+            return BadRequest("BranchCode is required.");
+        }
+
+        if (collStatusIds is null || collStatusIds.Count == 0)
+        {
+            return BadRequest("At least one CollStatusId must be provided.");
+        }
+
+        var result = await _collectionManager
+            .GetTodayCollectionsAsync(branchCode, clientId, collStatusIds, cancellationToken)
+            .ConfigureAwait(false);
+
+        return Ok(result);
     }
 }
+
--- DisputeAutomation/Program.cs
+++ DisputeAutomation/Program.cs
@@ -3,8 +3,14 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+// Application services & repositories
+builder.Services.AddScoped<BusinessLogicLayer.Manager.ICollectionManager, BusinessLogicLayer.Manager.CollectionManager>();
+builder.Services.AddScoped<BusinessLogicLayer.Processor.ICollectionProcessor, BusinessLogicLayer.Processor.CollectionProcessor>();
+builder.Services.AddScoped<DataAccessLayer.Repositories.ICollectionRepository, DataAccessLayer.Repositories.SqlServer.SqlServerCollectionRepository>();
 
 var app = builder.Build();
 
