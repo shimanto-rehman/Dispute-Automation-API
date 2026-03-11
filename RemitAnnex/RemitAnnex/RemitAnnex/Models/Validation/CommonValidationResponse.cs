e4cda6261b96f99123fe7f8c32c00379fc5f99ec
Shimanto Rehman <shimanto.rehman.bd@gmail.com>
2026-03-11 12:29:04 +0600

    Delete RemitAnnex/RemitAnnex directory

--- RemitAnnex/RemitAnnex/RemitAnnex/Models/Validation/CommonValidationResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace RemitAnnex.Models.Validation
-{
-    public class CommonValidationResponse
-    {
-        public bool IsValid { get; set; } = false;       
-        public string Msg { get; set; } = string.Empty;       
-    }
-}
--- RemitAnnex/RemitAnnex/RemitAnnex/Utility/Definition.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace RemitAnnex.Utility
-{
-    public sealed class Definition
-    {
-        public const string RAApiKey = @"7hfjdhgf&4#2.ljfgjdgutdf";
-    }
-}
